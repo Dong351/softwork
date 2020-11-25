a61adc7c375d1c2fd94c348e23d9eff9b0828c0a
minskiter <minskiter@qq.com>
2020-11-25 21:00:19 +0800

    [update] 更新聊天模块基本实现定义

--- chat5000/src/main/java/softwork/ChatMain5000.java
+++ chat5000/src/main/java/softwork/ChatMain5000.java
@@ -10,7 +10,9 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 @SpringBootApplication
 @MapperScan("softwork.mapper")
@@ -32,13 +34,46 @@ public class ChatMain5000 {
         }
         ModelAndView mav = new ModelAndView("/chat");
         mav.addObject("username", username);
-        mav.addObject("webSocketUrl", "ws://"+ InetAddress.getLocalHost().getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/webSocketOneToOne/3,4");
+        mav.addObject("webSocketUrl", "ws://"+ getLocalHostExactAddress().getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/single/3,4");
         return mav;
     }
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(ChatMain5000.class,args);
+    }
+
+    public static InetAddress getLocalHostExactAddress() {
+        try {
+            InetAddress candidateAddress = null;
+
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface iface = networkInterfaces.nextElement();
+                // 该网卡接口下的ip会有多个，也需要一个个的遍历，找到自己所需要的
+                for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = inetAddrs.nextElement();
+                    // 排除loopback回环类型地址（不管是IPv4还是IPv6 只要是回环地址都会返回true）
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了 就是我们要找的
+                            // ~~~~~~~~~~~~~绝大部分情况下都会在此处返回你的ip地址值~~~~~~~~~~~~~
+                            return inetAddr;
+                        }
+
+                        // 若不是site-local地址 那就记录下该地址当作候选
+                        if (candidateAddress == null) {
+                            candidateAddress = inetAddr;
+                        }
+
+                    }
+                }
+            }
+
+            // 如果出去loopback回环地之外无其它地址了，那就回退到原始方案吧
+            return candidateAddress == null ? InetAddress.getLocalHost() : candidateAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
